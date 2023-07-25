147e711aa0af41dc9c7c31806057a8f93a8092f3
AdCons <ad_con.reload@protonmail.ch>
2023-07-25 03:50:23 -0700

    feat: :monocle_face: add basic sql files

---
+++ database/insert_mayz_table.sql
@@ -0,0 +1,17 @@
+INSERT INTO 
+mayz (
+    title,
+    content,
+    published
+)
+VALUES
+    (
+    'Mayz Post 1',
+    'This is the first May as posts.',
+    TRUE
+), 
+    (
+    'Mayz Post 2',
+    'This is the second May as posts.',
+    TRUE
+);
