2dbd82e44fb796c28f9e86e9d96ec2e0f49440ac
AdCons <ad_con.reload@protonmail.ch>
2023-07-29 01:21:20 -0700

    refactor: :recycle: add models instead of schemas and remove useless exceptions

--- app/crud.py
+++ app/crud.py
@@ -1,65 +1,62 @@
 ''' CRUD Operations for database '''
 
 from sqlmodel import Session, select, SQLModel, engine
-try:
-    from .schemas import BaseModel
-except ImportError:
-    from schemas import BaseModel
 
 
-def insert_one(sql_engine: engine, model: SQLModel, schema: BaseModel):
-    ''' Add May to table '''
-    may = model(**schema.dict())
-    with Session(sql_engine) as session:
-        session.add(may)
+def insert_one(sql_eng: engine, table: SQLModel(table=True), model: SQLModel):
+    ''' Add one entry to table '''
+    with Session(sql_eng) as session:
+        new_row = table.from_orm(model)
+        session.add(new_row)
         session.commit()
-        session.refresh(may)
-    return may
+        session.refresh(new_row)
+        return new_row
 
 
-def select_all(sql_engine: engine, model: SQLModel):
-    ''' Select all rows from database '''
-    with Session(sql_engine) as session:
-        query = session.exec(select(model)).all()
-    return query
+def select_all(sql_eng: engine, table: SQLModel(table=True)):
+    ''' Select all rows from table '''
+    with Session(sql_eng) as session:
+        all_rows = session.exec(select(table)).all()
+        return all_rows
 
 
-def select_id(sql_engine: engine, model: SQLModel, value: int):
-    ''' Select one May from database '''
-    with Session(sql_engine) as session:
-        query = session.get(model, value)
-    return query
-
-
-def select_latest(sql_engine: engine, model: SQLModel):
+def select_latest(sql_eng: engine, table: SQLModel(table=True)):
     ''' Select all Mayz from database '''
-    with Session(sql_engine) as session:
-        query = session.exec(
-            select(model).order_by(model.created_at.desc())).first()
-    return query
+    with Session(sql_eng) as session:
+        query = select(table).order_by(table.created_at.desc())
+        latest_row = session.exec(query).first()
+        return latest_row
+
+
+def select_id(sql_eng: engine, table: SQLModel(table=True), value: int):
+    ''' Select one May from database '''
+    with Session(sql_eng) as session:
+        query = session.get(table, value)
+        return query
 
 
 def update_id(
-    sql_engine: engine, model: SQLModel, value: int, schema: BaseModel
+    sql_eng: engine, table: SQLModel(table=True), model: SQLModel, index: int
 ):
     ''' Select one May from database '''
-    with Session(sql_engine) as session:
-        query = session.get(model, value)
-        if query:
-            query.title = schema.title
-            query.content = schema.content
-            session.add(query)
+    with Session(sql_eng) as session:
+        edited_row = session.get(table, index)
+        if edited_row:
+            new_row = model.dict(exclude_unset=True)
+            for key, value in new_row.items():
+                setattr(edited_row, key, value)
+            session.add(edited_row)
             session.commit()
-            session.refresh(query)
-    return query
+            session.refresh(edited_row)
+        return edited_row
 
 
-def delete_id(sql_engine: engine, model: SQLModel, value: int):
+def delete_id(sql_engine: engine, table: SQLModel(table=True), value: int):
     ''' Select one May from database '''
     with Session(sql_engine) as session:
-        query = session.get(model, value)
-        if query:
-            session.delete(query)
+        deleted_row = session.get(table, value)
+        if deleted_row:
+            session.delete(deleted_row)
             session.commit()
             return True
-    return False
+        return False
