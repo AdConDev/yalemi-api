afd1636197baca141da50e6da3dd902fb22123b0
AdCons <ad_con.reload@protonmail.ch>
2023-07-30 05:58:35 -0700

    refactor: :coffin: remove useless function

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 ''' Database connection and setup using SQLModel '''
 
-from sqlmodel import create_engine, Session, engine
+from sqlmodel import create_engine
 try:
     from . import models
 except ImportError:
@@ -12,31 +12,9 @@ def new_engine():
     return create_engine(POSTGRES_URL, echo=True)
 
 
-def create_db(sql_engine: engine):
+def create_db(sql_engine):
     ''' Create database and tables '''
     models.SQLModel.metadata.create_all(sql_engine)
-
-
-def create_mockup_mayz(sql_engine: engine):
-    ''' Add mockup rows to Mayz table '''
-    may_1 = models.May(
-        title="May 1st",
-        content="May the 1st be with you"
-        )
-    may_2 = models.May(
-        title="May 2nd",
-        content="May the 2nd be with you",
-        published=False
-        )
-    may_3 = models.May(
-        title="May 3rd",
-        content="May the 3rd be with you"
-        )
-    with Session(sql_engine) as session:
-        session.add(may_1)
-        session.add(may_2)
-        session.add(may_3)
-        session.commit()
 
 
 POSTGRES_FILE = "yalemi-dev"
