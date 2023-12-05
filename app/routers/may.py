88c05afdb72203c4eda6568e4549727fa983875e
AdCons <adrian.constante97@outlook.com>
2023-12-05 09:04:17 -0700

    refactor: :heavy_minus_sign: remove crud file dependency

--- app/routers/may.py
+++ app/routers/may.py
@@ -1,16 +1,18 @@
+# pylint: disable=E1101
 ''' Router for Mayz '''
 
 from typing import Annotated
 from fastapi import APIRouter, status, HTTPException, Depends
+from sqlmodel import Session, select
 from app.models import May, MayCreate, MayRead, MayUpdate, User
-from app import crud, oauth2
+from app import oauth2, database as db
 
 
 auth_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
-    )
+        )
 
 router = APIRouter(
     prefix="/may",
@@ -20,24 +22,32 @@ router = APIRouter(
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=MayRead)
 def post_one_may(
-    new_may: MayCreate,
-    current_user: Annotated[User, Depends(oauth2.get_current_active_user)]
+    *,
+    create_may: MayCreate,
+    current_user: Annotated[User, Depends(oauth2.get_current_active_user)],
+    session: Session = Depends(db.get_session)
 ):
     ''' Create a may '''
     if not current_user:
         raise auth_exception
-    created_may = crud.insert_one(May, new_may)
+    new_may = May(user_id=current_user.id, **create_may.dict())
+    created_may = May.from_orm(new_may)
+    session.add(created_may)
+    session.commit()
+    session.refresh(created_may)
     return created_may
 
 
 @router.get("/", response_model=list[MayRead])
 def get_all_may(
-    current_user: Annotated[User, Depends(oauth2.get_current_active_user)]
+    *,
+    current_user: Annotated[User, Depends(oauth2.get_current_active_user)],
+    session: Session = Depends(db.get_session)
 ):
     ''' Get all Mayz '''
     if not current_user:
         raise auth_exception
-    all_mayz = crud.select_all(May)
+    all_mayz = session.exec(select(May)).all()
     if not all_mayz:
         raise HTTPException(
             status_code=status.HTTP_204_NO_CONTENT,
@@ -48,12 +58,15 @@ def get_all_may(
 
 @router.get("/latest/", response_model=MayRead)
 def get_latest_may(
-    current_user: Annotated[User, Depends(oauth2.get_current_active_user)]
+    *,
+    current_user: Annotated[User, Depends(oauth2.get_current_active_user)],
+    session: Session = Depends(db.get_session)
 ):
     ''' Get latest may '''
     if not current_user:
         raise auth_exception
-    latest_may = crud.select_latest(May)
+    query = select(May).order_by(May.created_at.desc())  # type: ignore
+    latest_may = session.exec(query).first()
     if not latest_may:
         raise HTTPException(
             status_code=status.HTTP_204_NO_CONTENT,
@@ -62,54 +75,65 @@ def get_latest_may(
     return latest_may
 
 
-@router.get("/{id_may}/", response_model=MayRead)
+@router.get("/{may_id}/", response_model=MayRead)
 def get_one_may(
-    id_may: int,
-    current_user: Annotated[User, Depends(oauth2.get_current_active_user)]
+    may_id: int,
+    current_user: Annotated[User, Depends(oauth2.get_current_active_user)],
+    session: Session = Depends(db.get_session)
 ):
     ''' Get specific may '''
     if not current_user:
         raise auth_exception
-    one_may = crud.select_id(May, id_may)
+    one_may = session.get(May, may_id)
     if not one_may:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"No May with ID {id_may} found"
+            detail=f"No May with ID {may_id} found"
             )
     return one_may
 
 
 @router.put(
-    "/{id_may}/", status_code=status.HTTP_202_ACCEPTED,
+    "/{may_id}/", status_code=status.HTTP_202_ACCEPTED,
     response_model=MayRead)
 def put_one_may(
-    id_may: int,
-    updated_may: MayUpdate,
-    current_user: Annotated[User, Depends(oauth2.get_current_active_user)]
+    may_id: int,
+    may_update: MayUpdate,
+    current_user: Annotated[User, Depends(oauth2.get_current_active_user)],
+    session: Session = Depends(db.get_session)
 ):
     ''' Update specific may '''
     if not current_user:
         raise auth_exception
-    edited_may = crud.update_id(May, updated_may, id_may)
+    edited_may = session.get(May, may_id)
     if not edited_may:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"No May with ID {id_may} found"
+            detail=f"No May with ID {may_id} found"
             )
+    new_may = may_update.dict(exclude_unset=True)
+    for key, value in new_may.items():
+        setattr(edited_may, key, value)
+    session.add(edited_may)
+    session.commit()
+    session.refresh(edited_may)
     return edited_may
 
 
-@router.delete("/{id_may}/", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{may_id}/", status_code=status.HTTP_204_NO_CONTENT)
 def delete_one_may(
-    id_may: int,
-    current_user: Annotated[User, Depends(oauth2.get_current_active_user)]
+    may_id: int,
+    current_user: Annotated[User, Depends(oauth2.get_current_active_user)],
+    session: Session = Depends(db.get_session)
 ):
     ''' Delete specific may '''
     if not current_user:
         raise auth_exception
-    deleted_may = crud.delete_id(May, id_may)
-    if not deleted_may:
+    deleted_row = session.get(May, may_id)
+    if not deleted_row:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"No mayz with this id {id_may}"
+            detail=f"No mayz with this id {may_id}"
             )
+    session.delete(deleted_row)
+    session.commit()
