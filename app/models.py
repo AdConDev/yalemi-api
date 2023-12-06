596c729ea42b9a47f412c40b5f8e0f57539f5be9
AdCons <adrian.constante97@outlook.com>
2023-12-06 02:46:41 -0700

    feat: :sparkles: add read to relationship through orm

--- app/models.py
+++ app/models.py
@@ -8,37 +8,18 @@ from sqlmodel import (
     )
 
 
-class UserBase(SQLModel):
-    ''' Defining the User Base Model '''
-    nickname: str = Field(max_length=25, nullable=False)
-
-
-class UserUpdate(SQLModel):
-    ''' Defining the User Update Model '''
-    nickname: str | None = None
-    email: EmailStr | None = None
-    username: str | None = None
-    password: str | None = None
-    enabled: bool | None = None
-
-
-class UserCreate(UserBase):
+class UserCreate(SQLModel):
     ''' Defining the User Create Model '''
+    nickname: str | None = Field(max_length=25, nullable=True)
     username: str = Field(
-        max_length=15, nullable=False, unique=True, index=True)
+        max_length=15, nullable=False, unique=True, index=True
+        )
     email: EmailStr = Field(nullable=False, unique=True)
     password: str = Field(max_length=64, nullable=False)
 
 
-class UserData(UserBase):
-    ''' Defining the User Data Model '''
-    username: str = Field(
-        max_length=15, nullable=False, unique=True, index=True)
-    email: EmailStr = Field(nullable=False, unique=True, index=True)
-
-
-class UserMetadata(SQLModel):
-    ''' Defining the User Metadata Model '''
+class User(UserCreate, table=True):
+    ''' Defining the User Model '''
     id: int | None = Field(default=None, primary_key=True)
     created_at: datetime = Field(
         sa_column=Column(
@@ -52,35 +33,43 @@ class UserMetadata(SQLModel):
             server_default='TRUE', nullable=False
         ),
     )
-
-
-class UserRead(UserMetadata, UserData):
-    ''' Defining the User Read Model '''
-
-
-class User(UserRead, table=True):
-    ''' Defining the User Model '''
-    password: str = Field(max_length=64, nullable=False)
-    mayz: List["May"] = Relationship(
+    mayz: List['May'] = Relationship(
         back_populates="user",
         sa_relationship_kwargs={"cascade": "all,delete,delete-orphan"}
         )
 
 
-class MayData(SQLModel):
-    ''' Defining the May Data Model '''
+class UserMay(SQLModel):
+    ''' Defining the User Data Model '''
+    id: int | None = None
+    nickname: str | None = None
+    username: str | None = None
+    enabled: bool | None = None
+
+
+class UserUpdate(SQLModel):
+    ''' Defining the User Update Model '''
+    nickname: str | None = None
+    username: str | None = None
+    enabled: bool | None = None
+    email: EmailStr | None = None
+    password: str | None = None
+
+
+class UserRead(UserMay):
+    ''' Defining the User Read Model '''
+    created_at: datetime | None = None
+    mayz: List['MayUser'] | None = None
+
+
+class MayCreate(SQLModel):
+    ''' Defining the May Create Model '''
     title: str = Field(max_length=30, index=True, nullable=False)
     content: str = Field(max_length=150, nullable=False)
-    published: bool | None = Field(
-        sa_column=Column(
-            Boolean(create_constraint=True),
-            server_default='TRUE', nullable=False
-            ),
-        )
 
 
-class MayMetadata(SQLModel):
-    ''' Defining the May Metadata Model '''
+class May(MayCreate, table=True):
+    ''' Defining the May Model '''
     id: int | None = Field(default=None, primary_key=True)
     created_at: datetime | None = Field(
         sa_column=Column(
@@ -90,30 +79,31 @@ class MayMetadata(SQLModel):
             ),
         default=None)
     likes: int | None = Field(default=0)
-
-
-class MayCreate(MayData):
-    ''' Defining the May Create Model '''
-
-
-class MayRead(MayData, MayMetadata):
-    ''' Defining the May Model '''
     user_id: int | None = Field(
         nullable=False,
         foreign_key='user.id',
         )
+    user: User | None = Relationship(back_populates="mayz")
+
+
+class MayUser(SQLModel):
+    ''' Defining the May User Model '''
+    id: int | None = None
+    title: str | None = None
+    content: str | None = None
 
 
 class MayUpdate(SQLModel):
     ''' Defining the May Update Model '''
     title: str | None = None
     content: str | None = None
-    published: bool | None = None
 
 
-class May(MayRead, table=True):
-    ''' Defining the May Model '''
-    user: User | None = Relationship(back_populates="mayz")
+class MayRead(MayUser):
+    ''' Defining the May Data Model '''
+    created_at: datetime | None = None
+    likes: int | None = None
+    user: UserMay | None = None
 
 
 class Token(SQLModel):
@@ -126,3 +116,6 @@ class TokenData(SQLModel):
     ''' Defining the Token Data Model '''
     username: str
     email: str
+
+
+UserRead.update_forward_refs()
